e9aa2303a24db971481e30882cd2d4cb460386f6
EruDev <1027926875@qq.com>
2018-01-14 20:19:06 +0800

    06

---
+++ 前端/06/test.py
@@ -0,0 +1,9 @@
+print("我爱小甲鱼")
+temp = input("不妨猜一猜小甲鱼心里想的是哪个数字？")
+guess = int(temp)
+if guess == 8:
+	print("握草，你是小甲鱼心里的蛔虫么？")
+	print("哼，猜中了也没奖励")
+else:
+    print("猜错啦小甲鱼心里想的是8")
+print("游戏结束不玩了")          
